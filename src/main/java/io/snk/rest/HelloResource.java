fcd60e030c10c652836297548a94b22d2b191ad2
nguyenntt <nguyenntt3@vng.com.vn>
2021-06-19 18:42:12 +0700

    open permission

--- src/main/java/io/snk/rest/HelloResource.java
+++ src/main/java/io/snk/rest/HelloResource.java
@@ -3,9 +3,12 @@ package io.snk.rest;
 import restx.annotations.GET;
 import restx.annotations.RestxResource;
 import restx.factory.Component;
+import restx.security.PermitAll;
 
-@Component @RestxResource
+@Component
+@RestxResource
 public class HelloResource {
+    @PermitAll
     @GET("/message")
     public String sayHello(String who) {
         return "Hello";
